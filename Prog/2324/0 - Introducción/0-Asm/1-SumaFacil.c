c2409437b1e5dacf84ef23f2f9cc2d0c696918ab
Víctor de Juan <dejuan.victor@gmail.com>
2024-09-17 10:56:26 +0200

    Asignatura de Programación

---
+++ Prog/2324/0 - Introducción/0-Asm/1-SumaFacil.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+	
+	int numero1,numero2,numero3;
+
+	numero1 = 5;
+	numero2 = 2;
+	numero3 = numero1 * numero2;
+
+	printf("%f",sqrt(numero3));
+
+	return EXIT_SUCCESS;
+}
---
+++ Prog/2324/1 - Aprendiendo C/Práctica2/SumaLetras.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	
+	char letra = 'a';
+
+	printf("Caracter: %c\n",letra+1);
+	printf("Entero: %i\n",letra+1);
+
+	return EXIT_SUCCESS;
+}
