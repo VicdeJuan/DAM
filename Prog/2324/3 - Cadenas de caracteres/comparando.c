751c853946e67855b3ea3f27e632a598578a7c47
Vicdejuan <dejuan.victor@gmail.com>
2024-10-29 14:34:21 +0100

    Ejercicios de hoy

---
+++ Prog/2324/3 - Cadenas de caracteres/comparando.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*
+*	Autor: Víctor de Juan
+*/
+
+int main(){
+	char str1[8] = "iguales";
+	char str2[8] = "iguales";
+	
+	char str3[8];
+
+	if (str1 == str2){
+		printf("Son iguales\n");
+	} else{
+		printf("Son distintas\n");
+	}
+
+	if (str1 < str2){
+		printf("str1 es menor\n");
+	} else{
+		printf("str2 es menor\n");
+	}
+
+	// Comparar caracter a caracter
+	for (int i=0; i<8; i++){
+		if (str1[i] == str2[i]){
+			printf("%c",str1[i]);
+		}
+	}
+	printf("\n");
+	
+	str3 = str1;
+	str1[0] = 'I';
+	printf("%c",str3[0]);	
+
+	return EXIT_SUCCESS;
+}
